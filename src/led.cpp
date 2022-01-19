0f431201fdfe25b0415cec78b92ec375a56cb718
Chun Ting, Liu <33743577+ConradLiuuu@users.noreply.github.com>
2022-01-19 20:28:55 +0800

    Update led.cpp
    improve code on 2022-01-19

--- src/led.cpp
+++ src/led.cpp
@@ -9,43 +9,47 @@ int i;
 void blink_callback (const std_msgs::Int8::ConstPtr& msg)
 {
   //ROS_INFO("Please publish 0-2 bool data to led_blink");
-  if (msg -> data == 0)
+  if (msg->data == 0)
   {
     digitalWrite (LED,HIGH);
     ROS_INFO("LED ON");
   }
-  if (msg -> data == 1)
+  else if (msg->data == 1)
   {
     digitalWrite (LED,LOW);
     ROS_INFO("LED OFF");
   }
-  if (msg -> data == 2)
+  else if (msg->data == 2)
   {
-	ROS_INFO("LED blinks ten times");
+    ROS_INFO("LED blinks ten times");
+    
     for (i = 0; i < 10; i++)
     {
-    digitalWrite(LED,HIGH);
-	ROS_INFO("LED ON");
-    delay(500);
-    digitalWrite(LED,LOW);
-	ROS_INFO("LED OFF");
-    delay(500);    
+      digitalWrite(LED,HIGH);
+      ROS_INFO("LED ON");
+      delay(500);
+    
+      digitalWrite(LED,LOW);
+      ROS_INFO("LED OFF");
+      delay(500);    
     }
   }
 }
 
 int main(int argc,char** argv)
 {
-  ros::init(argc ,argv ,"blink");
+  ros::init(argc, argv, "blink");
   ROS_INFO("Start RPI LED Blink node");
   wiringPiSetup();
-  pinMode(LED,OUTPUT);
+  pinMode(LED, OUTPUT);
 
-  ros::NodeHandle n;
+  ros::NodeHandle nh;
+	
   double freq;
-  n.getParam("/print_freq",freq);
-  ros::Subscriber sub = n.subscribe("sensorr",10,blink_callback);
-  //led_blink is topic name
+  nh.getParam("/print_freq", freq);
+  
+  ros::Subscriber sub = nh.subscribe("sensor", 10, blink_callback);
+ 
   ros::Duration(freq).sleep();  
   ros::spin();
 }
